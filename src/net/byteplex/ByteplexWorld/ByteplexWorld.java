6f7e4487bee3ae0fd3b7bc7488073b2342efb2da
BuildTools <ckhawks@gmail.com>
2018-03-12 02:13:24 -0500

    initial plugin setup

---
+++ src/net/byteplex/ByteplexWorld/ByteplexWorld.java
@@ -0,0 +1,16 @@
+package net.byteplex.ByteplexWorld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ByteplexWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}

2e69bb30477350b0feed1a5abd3553c4e66fb609
DeadN0tSleeping <eon979@gmail.com>
2018-03-27 19:53:42 -0500

    Made a prototype for removing recipes

--- src/net/byteplex/ByteplexWorld/ByteplexWorld.java
+++ src/net/byteplex/ByteplexWorld/ByteplexWorld.java
@@ -1,17 +1,20 @@
 package net.byteplex.ByteplexWorld;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import net.byteplex.ByteplexCore.ChatFormat;
+import net.byteplex.ByteplexCore.util.ChatFormat;
 
 public class ByteplexWorld extends JavaPlugin {
 
     @Override
     public void onEnable(){
-
+        this.getServer().getPluginManager().registerEvents(new RemoveRecipes(), this);
     }
 
     @Override
     public void onDisable(){
 
     }
+
 }
